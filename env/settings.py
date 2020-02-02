303591d1775d20b94dae6fdfe3326238efeeb73f
vodbeke1 <vodbeke1@gmail.com>
2020-02-02 12:09:33 +0100

    Added parent class to configuration

--- env/settings.py
+++ env/settings.py
@@ -4,16 +4,16 @@ from pathlib import Path
 
 env_path = Path(".") / ".env"
 
-class DevelopmentConfiguration:
+class Configuration:
     DEBUG = os.getenv("DEBUG").lower() == "true"
     ENV = os.getenv("ENV")
+
+class DevelopmentConfiguration(Configuration):
     load_dotenv(dotenv_path=env_path)
 
-class TestConfiguration:
-    DEBUG = os.getenv("DEBUG").lower() == "true"
-    ENV = os.getenv("ENV")
+class TestConfiguration(Configuration):
+    pass
 
-class ProductionConfiguration:
-    DEBUG = os.getenv("DEBUG").lower() == "true"
-    ENV = os.getenv("ENV")
+class ProductionConfiguration(Configuration):
+    pass
 
